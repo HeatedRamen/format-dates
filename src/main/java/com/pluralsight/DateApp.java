2a494db3f411ecd59a338fd2808686bba4069bef
ramenmac <kevinnguyen4200@gmail.com>
2026-04-22 11:26:37 -0500

    Clean up

--- src/main/java/com/pluralsight/DateApp.java
+++ src/main/java/com/pluralsight/DateApp.java
@@ -3,34 +3,32 @@ package com.pluralsight;
 import java.time.ZonedDateTime;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
 
 public class DateApp {
     public static void main(String[] args) {
 
-        LocalDateTime timeNow = LocalDateTime.now(ZoneOffset.UTC);
+        ZonedDateTime timeNow = LocalDateTime.now().atZone(ZoneId.systemDefault());
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
-        System.out.println(timeNow.format(formatter));
+        DateTimeFormatter firstFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        System.out.println(timeNow.format(firstFormatter));
 
-        formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        System.out.println(timeNow.format(formatter));
+        DateTimeFormatter secondFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        System.out.println(timeNow.format(secondFormatter));
 
-        formatter = DateTimeFormatter.ofPattern("EEEE dd, yyyy");
-        System.out.println(timeNow.format(formatter));
-
-        // Local Time
-        formatter = DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy\tHH:mm")
-                                     .withZone(ZoneId.of("GMT"));
-        System.out.println(timeNow.format(formatter));
+        DateTimeFormatter thirdFormatter = DateTimeFormatter.ofPattern("EEEE dd, yyyy");
+        System.out.println(timeNow.format(thirdFormatter));
 
         // GMT time
-        ZonedDateTime todayCT = LocalDateTime.now().atZone(ZoneId.of("America/Chicago"));
-        formatter = DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy\tHH:mm")
+        DateTimeFormatter fourthFormatter = DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy\tHH:mm")
+                                     .withZone(ZoneId.of("GMT"));
+        System.out.println(timeNow.format(fourthFormatter));
+
+        // Central Time
+        DateTimeFormatter fifthFormatter = DateTimeFormatter.ofPattern("EEEE, MMM dd, yyyy\tHH:mm")
                                      .withZone(ZoneId.of("America/Chicago"));
-        System.out.println(todayCT.format(formatter));
+        System.out.println(timeNow.format(fifthFormatter));
 
     }
 
